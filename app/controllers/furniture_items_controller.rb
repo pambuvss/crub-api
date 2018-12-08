1c61d01a8fed9e8a779c85000557b2c060518437
Vitaliy Suprunov <pambuvss@gmail.com>
2018-12-08 04:32:42 +0200

    Change Access-Control

--- app/controllers/furniture_items_controller.rb
+++ app/controllers/furniture_items_controller.rb
@@ -45,10 +45,10 @@ class FurnitureItemsController < ApplicationController
 
 
 	private
-		def cors_set_access_control_headers
+	def cors_set_access_control_headers
 			headers['Access-Control-Allow-Origin'] = '*'
-			headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+			headers['Access-Control-Allow-Methods'] = '*'
 			headers['Access-Control-Request-Method'] = '*'
-			headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+			headers['Access-Control-Allow-Headers'] = '*'
 	  	end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,8 +19,8 @@ class SessionsController < ApplicationController
 	private
 		def cors_set_access_control_headers
 			headers['Access-Control-Allow-Origin'] = '*'
-			headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+			headers['Access-Control-Allow-Methods'] = '*'
 			headers['Access-Control-Request-Method'] = '*'
-			headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+			headers['Access-Control-Allow-Headers'] = '*'
 	  	end
 end
